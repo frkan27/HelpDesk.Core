a4d8c0302775f8b3b9e7a1f1958432bc7c28fa0c
ismail Çadır <ismailcadir@gmail.com>
2019-03-27 11:26:35 +0300

    Sayfa Ekleme Öncesi

---
+++ HelpDesk.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelpDesk.BLL.Repository.Abstracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IRepoIdentity _userRoleRepo;
+
+        public AccountController(IRepoIdentity userRoleRepo)
+        {
+            _userRoleRepo = userRoleRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
