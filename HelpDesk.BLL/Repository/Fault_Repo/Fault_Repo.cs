57990e01ac6b7b0f5ccf3641efdbb4524ada740f
ismail Çadır <ismailcadir@gmail.com>
2019-04-01 09:54:28 +0300

    ok

---
+++ HelpDesk.BLL/Repository/Fault_Repo/Fault_Repo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HelpDesk.BLL.Repository.Abstracts;
+using HelpDesk.DAL;
+using HelpDesk.Model.Entities.Poco;
+
+namespace HelpDesk.BLL.Repository.Fault_Repo
+{
+    public class Fault_Repo : RepoBase<FaultRecord, int>
+    {
+        private readonly MyContext _dbContext;
+
+        public Fault_Repo(MyContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+    }
+}
--- HelpDesk.WebUI/Controllers/CustomerController.cs
+++ HelpDesk.WebUI/Controllers/CustomerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HelpDesk.BLL;
+using HelpDesk.BLL.Repository.Fault_Repo;
 using HelpDesk.Model.ViewModels.FaultViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,19 +12,22 @@ namespace HelpDesk.WebUI.Controllers
     public class CustomerController : Controller
     {
         private readonly MembershipTools _membershipTools;
+        private readonly Fault_Repo _fault_repo;
 
-        public CustomerController(MembershipTools membershipTools)
+        public CustomerController(MembershipTools membershipTools, Fault_Repo fault_repo)
         {
             _membershipTools = membershipTools;
+            _fault_repo = fault_repo;
         }
         public IActionResult Index()
         {
+           
             return View();
         }
         [HttpPost]
         public IActionResult FaultAdd(FaultViewModels model)
         {
-
+            
             return View();
         }
     }
